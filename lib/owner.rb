105bb631fe66156913daa040b72f8b40c2119283
Mark May <mamark90@gmail.com>
2019-09-29 04:22:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,15 +2,16 @@ require 'pry'
 
 class Owner
 
-  attr_accessor :owner, :name, :mood, :fish, :cat, :dog, :pets
-  attr_reader :species
+  attr_accessor :mood
+  attr_reader :name
 
   @@all  = []
 
-  def initialize(name)
+  def initialize(name, owner)
     @name = name
+    @owner = owner
+    @mood = "nervous"
     @@all << self
-    @pets = {:cats=>[], :dogs=>[], :fishes=>[] }
   end
 
   def self.all
@@ -33,47 +34,3 @@ class Owner
     "I am a #{self.species}."
   end
 
-  def pets
-    @pets
-  end
-
-  def buy_fish(name)
-    @pets[:fishes] << Fish.new(name)
-  end
-
-  def buy_cat(name)
-    @pets[:cats] << Cat.new(name)
-  end
-
-  def buy_dog(name)
-    @pets[:dogs] << Dog.new(name)
-  end
-
-  def walk_dogs
-    self.pets[:dogs].each {|dog| dog.mood = "happy"}
-  end
-
-  def play_with_cats
-    self.pets[:cats].each {|cat| cat.mood = "happy"}
-  end
-
-  def feed_fish
-    self.pets[:fishes].each {|fish| fish.mood = "happy"}
-  end
-
-  def sell_pets
-    self.pets.collect do |type,values|
-      values.each do |attribute|
-        attribute.mood = "nervous"
-        end
-        values.clear
-      end
-    end
-
-    def list_pets
-      num_fish = self.pets[:fishes].count
-      num_dogs  = self.pets[:dogs].count
-      num_cats  = self.pets[:cats].count
-      "I have #{num_fish} fish, #{num_dogs} dog(s), and #{num_cats} cat(s)."
-    end
-  end
